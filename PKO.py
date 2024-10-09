000a103862f0c7a9d842ee78e0ea804fa0282bf0
VINagaev <15vlad19@gmail.com>
2024-10-09 16:26:04 +0700

    +
    +

---
+++ PKO.py
@@ -0,0 +1,100 @@
+# print("Hisssssss...")
+
+# print("Hello, World!")
+
+# print(" The itsy bitsy spider climbed up the waterspout.")
+# print("Down came the rain and washed the spider out.")
+
+# print("The itsy bitsy spider climbed upthe waterspout.")
+# print()
+# print("Down came the rain and washed the spider out.")
+
+# print("The itsy bitsy spider \nclimbed up the waterspout.")
+# print()
+# print("Down came the rain \nand washed the spider out.")
+
+# print("\") - ошибка
+# print("\\")
+
+# print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+# print("My name is", "Python.")
+# print("Monty Python")
+
+# print("My name is", "Python.", end=" ")
+# print("Monty Python")
+
+# print("My", "name", "is", "Monty", "Python.", sep="-")
+
+# print("My", "name", "is", sep="_", end="*")
+# print("Monty", "Python.", sep="*", end="*\n")
+
+# print("2") #строка
+# print(2)   #число
+
+# print(0o123)
+
+# print(0x123)
+
+# print(0.0000000000000000000001)
+
+# print("I like \"Monty Python\"")
+
+# print('I like "Monty Python"')
+
+# print("I like \'Monty Python\'")
+
+# print("I like 'Monty Python'")
+
+# print(True > False)
+# print(True < False)
+
+# print(2+2)
+
+# print (2 ** 3)
+# print (2 ** 3.)
+# print (2. ** 3)
+# print (2. ** 3.)
+
+# print(2 * 3)
+# print(2 * 3.)
+# print(2. * 3)
+# print(2. * 3.)
+
+# print(6 / 3)
+# print(6 / 3.)
+# print(6. / 3)
+# print(6. / 3.)
+
+# print(6 // 3)
+# print(6 // 3.)
+# print(6. // 3)
+# print(6. // 3.)
+
+# print(6 // 4)
+# print(6. // 4)
+
+# print(-6 // 4)
+# print(6. // -4)
+
+# print(14 % 4)
+
+# print(12 % 4.5)
+
+# print(6 / 0)
+
+# print(-4 + 4)
+# print(-4. + 8)
+
+# print(-4 - 4)
+# print(4. - 8)
+# print(-1.1)
+
+# print(+2)
+
+# print(9 % 6 % 2)
+
+# print(2 ** 2 ** 3)
+
+# print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+
