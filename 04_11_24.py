a7dcf3771c469cc4fd2a144d2892c9ec7056d7c3
VINagaev <15vlad19@gmail.com>
2024-11-04 20:20:55 +0700

    +

--- 04_11_24.py
+++ 04_11_24.py
@@ -5,7 +5,7 @@
 #     print(" "*20, "|", "*" * i if i <= 20 else "*" * (40 - i), " " * (40 - i) if i <=20 else " " * i, "|")
 #     i+=1
 # print(' ' * 20, '_' * 45)
-
+from numpy.linalg.lapack_lite import xerbla
 
 #E
 
@@ -21,17 +21,31 @@
 
 #ДЗ 13 задание 2
 
+# sp = [-1, -2, -4, -5, 0, 3, 5, 6, 63, 4]
+# pos = 0
+# neg = 0
+# nul = 0
+# for x in sp:
+#     if x < 0:
+#         neg += 1
+#     if x > 0:
+#         pos += 1
+#     if x == 0:
+#         nul += 1
+#     print(max(sp))
+#     print(min(sp))
+#     print(pos, neg, nul)
+
+
+
+
+
 sp = [-1, -2, -4, -5, 0, 3, 5, 6, 63, 4]
 pos = 0
 neg = 0
 nul = 0
 for x in sp:
-    if x < 0:
-        neg += 1
-    if x > 0:
-        pos += 1
-    if x == 0:
-        nul += 1
-    print(max(sp))
-    print(min(sp))
-    print(pos, neg, nul)
+    try:
+        print(s / x)
+    except:
+print("delenie na 0")
